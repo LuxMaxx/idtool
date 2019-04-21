27b58d6eec3420bfb32508edbfdd865d50a8ff82
Sultan Kautsar <42274355+svzax@users.noreply.github.com>
2019-04-21 14:06:56 +0700

    Add files via upload

---
+++ raiku.sh
@@ -0,0 +1,264 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+clear
+echo
+echo $red"           Tool akan di install dalam 5 tahapan"
+echo $red"           Sabaro yo cok, uripmu bakal penak :v"   
+sleep 1
+echo '''\a
+\033[32;1m                              _
+\033[32;1m                             / |
+\033[32;1m                             | |
+\033[32;1m                             | |
+\033[32;1m                             |_|'''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"           Tool akan di install dalam 5 tahapan"
+echo $red"           Sabaro yo cok, uripmu bakal penak :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"           Tool akan di install dalam 5 tahapan"
+echo $red"           Sabaro yo cok, uripmu bakal penak :v"
+sleep 1
+echo '''\a
+\033[32;1m                            _____
+\033[32;1m                           |___ /
+\033[32;1m                             |_ \
+\033[32;1m                            ___) |
+\033[32;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"           Tool akan di install dalam 5 tahapan"
+echo $red"           Sabaro yo cok, uripmu bakal penak :v"
+sleep 1
+echo '''\a
+\033[32;1m                            _  _
+\033[32;1m                           | || |
+\033[32;1m                           | || |_
+\033[32;1m                           |__   _|
+\033[32;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"           Tool akan di install dalam 5 tahapan"
+echo $red"           Sabaro yo cok, uripmu bakal penak :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            | ___|
+\033[32;1m                            |___ \
+\033[32;1m                             ___) |
+\033[32;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $cy"==================="$cy" ==================="
+echo $i"XXX"$pu"  AUTHOR  :  SVZAX  "$i"                   XXX"
+echo $i"XXX"$pu"  WhatsApp:  08122545xxxx"$i"              XXX"
+echo $i"XXX"$pu"  GMAIL   :  svnteentthones@gmail.com"$i"  XXX"
+echo $i"XXX"$pu"  IG      :  @drivtomev"$i"                XXX"
+echo $cy"==================="$cy" ==================="
+echo
+echo $cy"MILEH NYOH:"
+echo
+echo $ku"["$me"1"$ku"]"$i" LAPOR FESBUK"
+echo $ku"["$me"2"$ku"]"$i" NGOCOK PASSWORD"
+echo $ku"["$me"3"$ku"]"$i" NGOCOK PASSWORD FESBUK NGANTI CROT"
+echo $ku"["$me"4"$ku"]"$i" MBF"
+echo $ku"["$me"5"$ku"]"$i" KLONING YAHOO"
+echo $ku"["$me"6"$ku"]"$i" OSIF"
+echo $ku"["$me"7"$ku"]"$i" PROPILGUARD FESBUK"
+echo $ku"["$me"8"$ku"]"$i" REAKSI BOT FESBUK"
+echo $ku"["$me"9"$ku"]"$i" SPAM TILPUN"
+echo $ku"["$me"10"$ku"]"$i" SPAM WHAT SAP"
+echo $ku"["$me"11"$ku"]"$i" YUTUB-DL"
+echo $ku"["$me"12"$ku"]"$i" NGLACAK PL"
+echo $i"["$me"13"$i"]"$ku" METU"
+echo
+echo $i"XXX---"$cy"Ketik nomere wae cok"$cy":"
+read -p"XXX---> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "S A B A R"|lolcat
+sleep 2
+echo $cy"Suwun yo cok, wes nganggo tools gawweanku"
+echo
+sleep 2
+echo $cy"Monggo di enggo mugo2 lancar cok"
+echo
+sleep 2
+echo $cy"Nek arep ngejak padu opo gelot pc yo cok"
+echo
+echo
+sleep 2
+echo $cy"==================="$cy" ==================="
+echo $ku"Facebook :"$i" Drivtomev"
+echo $ku"IG       :"$i" @drivtomev"
+echo $ku"WhatsApp :"$i" 08122545xxxx"
+echo $cy"==================="$cy" ==================="
+echo
+echo
+sleep 2
+echo $pur"Matur suwun sanget lur, wes gari penake to?"
+echo
+sleep 2
+echo $pur"Ndang Minggat"
+echo
+sleep 5
+exit
+fi
