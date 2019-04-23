a87c2a0419b68456fad5732defccf7bbc047c93d
Sultan Kautsar <42274355+svzax@users.noreply.github.com>
2019-04-23 08:35:45 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -6,7 +6,15 @@ cy='\033[36;1m' #cyan
 red='\033[31;1m' #red
 pe='\033[37;1m' #purple
 yw='\033[33;1m' #yellow
+
 sleep 2
+echo
+echo
+echo
+#Login
+python2 login-user.py
+sleep 3
+
 clear
 echo
 echo
@@ -84,37 +92,43 @@ echo '''\a
 sleep 2
 clear
 echo
-echo $yw"======================"$yw" ======================"
-echo $yw"XXX"$gre"  AUTHOR  :  SVZAX"$yw"                     XXX"
-echo $yw"XXX"$gre"  WhatsApp:  0812-2545-9071"$yw"            XXX"
-echo $yw"XXX"$gre"  GMAIL   :  svnteentthones@gmail.com"$yw"  XXX"
-echo $yw"XXX"$gre"  IG      :  @drivtomev"$yw"                XXX"
-echo $yw"======================"$yw" ======================"
+echo $yw"======================="$yw" ======================="
+echo $yw">"$gre"  AUTHOR     :  SVZAX"$yw""
+echo $yw">"$gre"  Builder OS :  Linux (UNIX)"$yw""
+echo $yw">"$gre"  Version    :  13.3.211"$yw""
+echo $yw">"$gre"  Lang       :  Shell(.sh) | Python, Pythonpy, Python2, Python3(.py)"$yw""
+echo $yw">"$gre"  Source     :  https://github.com/svzax/idtool"$yw""
+echo $yw">"$gre"  Country    :  INDONESIA (ID)"$yw""
+echo $yw"======================="$yw" ======================="
 echo
 echo $yw"Input Choice:"
 echo "\033[00mSome tools, are not work. We will fix it ASAP."
+echo "\033[00mIf you're not type input / some wrong input, it will auto exits."
 echo
 echo $gre"["$yw"1"$gre"]"$gre"  Report Facebook Account (With ID)"
 echo $gre"["$yw"2"$gre"]"$gre"  Brute Force"
 echo $gre"["$yw"3"$gre"]"$gre"  Brute Forcer for Facebook Account"
-echo $gre"["$yw"4"$gre"]"$gre"  (Multi) Brute Forcer for Facebook Account"
-echo $gre"["$yw"5"$gre"]"$gre"  Cloning Yahoo (Login with Yahoo Account)"
-echo $gre"["$yw"6"$gre"]"$gre"  Open Source Information Facebook"
-echo $gre"["$yw"7"$gre"]"$gre"  Profil Guard (Shield) Facebook"
-echo $gre"["$yw"8"$gre"]"$gre"  Bot React for Facebook"
-echo $gre"["$yw"9"$gre"]"$gre"  Spam Call"
-echo $gre"["$yw"10"$gre"]"$gre" Spam WhatsApp"
-echo $gre"["$yw"11"$gre"]"$gre" Youtube DL"
-echo $gre"["$yw"12"$gre"]"$gre" Locator"
-echo $gre"["$yw"13"$gre"]"$gre" Exit"
+echo $gre"["$yw"4"$gre"]"$gre"  Instainsane Brute Forcer"
+echo $gre"["$yw"5"$gre"]"$gre"  (Multi) Brute Forcer for Facebook Account"
+echo $gre"["$yw"6"$gre"]"$gre"  Cloning Yahoo (Login with Yahoo Account)"
+echo $gre"["$yw"7"$gre"]"$gre"  Open Source Information Facebook"
+echo $gre"["$yw"8"$gre"]"$gre"  Profil Guard (Shield) Facebook"
+echo $gre"["$yw"9"$gre"]"$gre"  Bot React for Facebook"
+echo $gre"["$yw"10"$gre"]"$gre" Spam Call"
+echo $gre"["$yw"11"$gre"]"$gre" Spam WhatsApp"
+echo $gre"["$yw"12"$gre"]"$gre" Youtube DL"
+echo $gre"["$yw"13"$gre"]"$gre" Locator"
+echo $gre"["$yw"14"$gre"]"$gre" Exit, info & Greeting About This Tools"
 echo
-echo $gre"XXX--->"$gre"Input Number"$yw":"
+echo "\033[00mType \033[32;1mv.update \033[00mto see update tools."
+echo
+echo $gre"XXX--->"$gre"Input Number \033[00m(1-14)"$yw":$gre"
 read -p "XXX---> " pil
 
 if [ $pil = 1 ]
 then
 clear
-figlet -f slant "1"|lolcat
+figlet -f slant "   1"|lolcat
 sleep 1
 git clone https://github.com/IlayTamvan/Report.git
 cd Report
@@ -125,7 +139,7 @@ fi
 if [ $pil = 2 ]
 then
 clear
-figlet -f slant "2"|lolcat
+figlet -f slant "   2"|lolcat
 sleep 1
 git clone https://github.com/Senitopeng/fbbrute.git
 cd fbbrute
@@ -145,17 +159,42 @@ fi
 if [ $pil = 4 ]
 then
 clear
-figlet -f slant "4"|lolcat
+figlet -f slant "   4"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/instainsane.git
+cd instainsane
+chmod +x instainsane.sh && chmod +x install.sh
+clear
+echo
+echo '\033[32;1mInstalling...'
+sleep 5
+clear
+echo
+echo '\033[32;1mAll Required, aovid to run...'
+sleep 9
+clear
+echo
+echo '\033[32;1mType "./install.sh && ./instainsane.sh" in ur terminal to run this program, or copying that commands.'
+sleep 5
+sudo su
+sleep 1
+exit
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "   5"|lolcat
 sleep 1
 git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
 cd multi-bruteforce-facebook
 python2 MBF.py
 fi
 
-if [ $pil = 5 ]
+if [ $pil = 6 ]
 then
 clear
-figlet -f slant "5"|lolcat
+figlet -f slant "   6"|lolcat
 sleep 1
 git clone https://github.com/wahyuandhika/YahooCloning.git
 cd YahooCloning
@@ -164,10 +203,10 @@ pip2 install requirements
 python2 cloning.py
 fi
 
-if [ $pil = 6 ]
+if [ $pil = 7 ]
 then
 clear
-figlet -f slant "6"|lolcat
+figlet -f slant "   7"|lolcat
 sleep 1
 git clone https://github.com/CiKu370/OSIF.git
 cd OSIF
@@ -175,86 +214,89 @@ pip2 install -r requirements.txt
 python2 osif.py
 fi
 
-if [ $pil = 7 ]
+if [ $pil = 8 ]
 then
 clear
-figlet -f slant "7"|lolcat
+figlet -f slant "   8"|lolcat
 sleep 1
 git clone https://github.com/jaxBCD/FBshield.git
 cd FBshield
 python2 guard.py
 fi
 
-if [ $pil = 8 ]
+if [ $pil = 9 ]
 then
 clear
-figlet -f slant "8"|lolcat
+figlet -f slant "   9"|lolcat
 sleep 1
 git clone https://github.com/edi-nasa12/ROBOT.git
 cd ROBOT
 python2 blackbot.py
 fi
 
-if [ $pil = 9 ]
+if [ $pil = 10 ]
 then
 clear
-figlet -f slant "9"|lolcat
+figlet -f slant "   1 0"|lolcat
 sleep 1
 git clone https://github.com/joss24242/prank
 cd prank
 php call.php
 fi
 
-if [ $pil = 10 ]
+if [ $pil = 11 ]
 then
 clear
-figlet -f slant "1 0"|lolcat
+figlet -f slant "   1 1"|lolcat
 sleep 1
 git clone https://github.com/4L13199/LITESPAM.git
 cd LITESPAM
 sh LITESPAM.sh
 fi
 
-if [ $pil = 11 ]
+if [ $pil = 12 ]
 then
 clear
-figlet -f slant "1 1"|lolcat
+figlet -f slant "   1 2"|lolcat
 sleep 1
 git clone https://github.com/rahmadxyz/sub-bot.git
 cd sub-bot
 php sub-bot.php
 fi
 
-if [ $pil = 12 ]
+if [ $pil = 13 ]
 then
 clear
-figlet -f slant "1 2"|lolcat
+figlet -f slant "   1 3"|lolcat
 sleep 1
 git clone https://github.com/thelinuxchoice/locator.git
 cd locator
 bash locator.sh
 fi
 
-if [ $pil = 13 ]
+if [ $pil = 14 ]
 then
 clear
-figlet -f slant "EXIT"|lolcat
+figlet -f slant "   EXIT"|lolcat
 sleep 2
 echo $gre"Thanks for using my Script!"
 echo
 sleep 2
-echo $gre"Hope you enjoy it!"
+echo $gre"Script shell is created on : March 30, 2019"
+echo $gre"@ 7.21 AM | INDONESIA (ID)"
 echo
 sleep 2
 echo $gre"Contact Me!"
 echo
 echo
 sleep 2
-echo $yw"==================="$yw" ==================="
-echo $yw"Facebook :"$gre" Drivtomev"
-echo $yw"IG       :"$gre" @drivtomev"
-echo $yw"WhatsApp :"$gre" 0812-2545-9071"
-echo $yw"==================="$yw" ==================="
+echo $yw"======================="$yw" ======================="
+echo $yw"XXX"$gre"  AUTHOR    :  SVZAX"$yw"                     XXX"
+echo $yw"XXX"$gre"  WhatsApp  :  0812-2545-9071"$yw"            XXX"
+echo $yw"XXX"$gre"  GMAIL     :  svnteentthones@gmail.com"$yw"  XXX"
+echo $yw"XXX"$gre"  Facebook  :  Drivtomev"$yw"                 XXX"
+echo $yw"XXX"$gre"  IG        :  @drivtomev"$yw"                XXX"
+echo $yw"======================="$yw" ======================="
 echo
 echo
 sleep 2
@@ -264,5 +306,16 @@ sleep 1
 echo $gre"Exiting Tools . . ."
 echo
 sleep 5
-exit
 fi
+
+# Another Shell
+if [ $pil = v.update ]
+then
+clear
+sleep 1
+sh tools-updates-23-APRIL-2019-6-54-AM.sh
+sleep 1
+fi
+
+#execute
+exit
