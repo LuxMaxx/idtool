207bd1bee3bbc807ab93206806c9365c69c8075a
Sultan Kautsar <42274355+svzax@users.noreply.github.com>
2019-05-01 11:50:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,29 +6,29 @@ cy='\033[36;1m' #cyan
 red='\033[31;1m' #red
 pe='\033[37;1m' #purple
 yw='\033[33;1m' #yellow
-sleep 2
+sleep 1
 echo
 echo
 echo
 python2 login-user.py
-sleep 3
+sleep 2
 clear
 echo
-echo '\033[32;1mLoading installer...'
-sleep 4
+echo '\033[32;1m Launching Updates...'
+sleep 2
 clear
 apt update && apt upgrade
 apt install nano
 apt install git
 pkg install python python2 vim figlet curl
-sleep 1
+sleep 2
 clear
 echo
-echo '\033[32;1mInstallation Completed.'
-sleep 4
+echo '\033[32;1m Update Successful.'
+sleep 2
 clear
 echo
-echo '\033[32;1mOpening Tools...'
-sleep 10
+echo '\033[32;1m Loading Tools...'
+sleep 2
 chmod +x tools.sh
 sh tools.sh
